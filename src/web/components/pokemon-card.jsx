66857120b233309eb58f7627680c174fbc782b48
Tim Mironov <timirlan666@gmail.com>
2018-10-08 14:02:33 +0500

    - old pokemon card

--- src/web/components/pokemon-card.jsx
+++ src/web/components/pokemon-card.jsx
@@ -7,23 +7,6 @@ import Chip from '@material-ui/core/Chip'
 import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 
-/*
-const PokemonCard = ({ pokemonItem }) => (
-  <Card
-    horizontal
-    className='grey lighten-4'
-    textClassName='grey-text text-darken-4'
-    header={
-      <CardTitle image={pokemonItem.coverUrl}/>
-    }
-    title={pokemonItem.name}
-  >
-    <div>{
-      pokemonItem.types.map(type => <Chip key={type}>{type}</Chip>)
-    }</div>
-  </Card>
-)
- */
 const PokemonCard = ({ pokemonItem }) => (
   <Card>
     <CardContent>
